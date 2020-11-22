1d9d44a7744d76a33a166ab2408ab7772a506cc6
Christopher Cavell <cdcavell@gmail.com>
2020-11-22 08:51:15 -0600

    v1 - Initial development

---
+++ README.md
@@ -0,0 +1,43 @@
+# allowed-branch-javascript-action
+JavaScript action returns success for pull request allowed branch, failure otherwise. 
+
+## Usage
+
+Create a new `.github/workflows/allowbranch.yml` file:
+
+```yaml
+on:
+  pull_request:
+    branches:
+      - master
+
+jobs:
+  allowed_branch_job:
+    runs-on: ubuntu-latest
+    name: Allowed Branch
+    steps:
+    # To use this repository's private action,
+    # you must check out the repository
+    - name: Checkout
+    uses: actions/checkout@v2
+    - name: Validate action step
+      id: allowed-branch
+      uses: cdcavell/allowed-branch-javascript-action@v1
+      with:
+        allowed-branch: 'develop'
+```
+
+Set Branch protection rules:
+
+![status checks](StatusChecks.jpg?raw=true)
+
+## Note
+
+`allowed-branch` is a delimited string containting allowed branches. String is comma delimited.
+Wildcard `*` is allowed, for example:
+
+`feature/*` will evaluate true for:
+- feature/v1
+- feature/v1.1
+- feature/v1.0.1
+
---
+++ index.js
@@ -0,0 +1,60 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+    // 'allowed-branch' input defined in action metadata file
+    const allowedBranchArray = core.getInput('allowed-branch').trim().split(",");
+    const pullRequestHeadRef = github.context.payload.pull_request.head.ref;
+    console.log(`allowed-branch: [${allowedBranchArray.toString()}]`);
+    console.log(`pull_request.head.ref: ${pullRequestHeadRef}`);
+
+    var isValid = false;
+
+    if (typeof pullRequestHeadRef === 'undefined') {
+        core.setFailed('pullRequestHeadRef not defined');
+    } else if (pullRequestHeadRef === null) {
+        core.setFailed('pullRequestHeadRef not defined');
+    } else {
+
+        allowedBranchArray.forEach(function (item, index) {
+
+            var comparer = item.trim();
+            var branch = pullRequestHeadRef.trim();
+            if (comparer.startsWith("*")) {
+                if (branch.endsWith(comparer.replace('*', ''))) {
+                    isValid = true;
+                    console.log(`compared ${branch} ends with ${comparer} : passed`);
+                } else {
+                    console.log(`compared ${branch} ends with ${comparer} : failed`);
+                }
+            } else if (comparer.endsWith("*")) {
+                if (branch.startsWith(comparer.replace('*', ''))) {
+                    isValid = true;
+                    console.log(`compared ${branch} starts with ${comparer} : passed`);
+                } else {
+                    console.log(`compared ${branch} starts with ${comparer} : failed`);
+                }
+            } else {
+                if (branch.localeCompare(comparer) === 0) {
+                    isValid = true;
+                    console.log(`compared ${branch} equals ${comparer} : passed`);
+                } else {
+                    console.log(`compared ${branch} equals ${comparer} : failed`);
+                }
+            }
+        });
+
+    }
+
+    if (isValid) {
+        console.log(`Merging of ${pullRequestHeadRef} allowed`);
+    } else {
+        core.setFailed(`Merging of ${pullRequestHeadRef} not allowed`);
+    }
+
+} catch (error) {
+
+    core.setFailed(error.message);
+
+}
